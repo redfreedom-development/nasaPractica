e6b0fd4973c4b86192783da62585319091107655
Jesus Sierralaya <random.music.1234567890@gmail.com>
2024-11-28 18:12:38 +0100

    app funcionando finalizada 1.1

--- app/src/main/java/com/example/nasapractica/activities/DetailActivity.kt
+++ app/src/main/java/com/example/nasapractica/activities/DetailActivity.kt
@@ -1,22 +1,20 @@
 package com.example.nasapractica.activities
 
-import android.content.Intent
-import android.os.Binder
+
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
-import android.util.Log
-import android.view.View
+
+
+
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 import com.example.nasapractica.R
 import com.example.nasapractica.data.DatosNasa
 import com.example.nasapractica.database.NasaDAO
 import com.example.nasapractica.databinding.ActivityDetailBinding
-import com.example.nasapractica.databinding.ActivityMainBinding
+
 import com.squareup.picasso.Picasso
 
 class DetailActivity : AppCompatActivity() {
@@ -56,17 +54,17 @@ class DetailActivity : AppCompatActivity() {
 
 
 
-        Log.d("Lifecycle", "HAS LLAMADO AL ONCREATE");
+
 
         mostrar_datos_details(datosNasa)
 
         comprobar_si_existe_ya_en_database(datosNasa)
 
-        binding.menuGrabar.setOnClickListener(){
+        binding.menuGrabar.setOnClickListener{
             mostrar_cuadro_dialogo_grabar(datosNasa)
         }
 
-        binding.menuBorrar.setOnClickListener(){
+        binding.menuBorrar.setOnClickListener{
 
             mostrar_cuadro_dialogo_delete(datosNasa)
         }
